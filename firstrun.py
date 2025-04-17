f6efa99b71470acf4bdc150282954318d0c36361
CRIMSY <rajarshiaxel@gmail.com>
2025-04-17 07:32:50 -0700

    1

--- firstrun.py
+++ firstrun.py
@@ -1,15 +1,15 @@
 from ultralytics import YOLO
 
-# Load the YOLOv8s model
+# Load the YOLOv8s model and force it to use CUDA
 model = YOLO('yolov8s.pt')
 
 # Train the model
 model.train(
     data=r'f:/REPO/FoodApp/data/UEC_Food_100/yolo/data.yaml',
-    epochs=100,                # You can adjust the number of epochs
-    imgsz=640,                 # Image size (default 640)
-    batch=16,                  # Batch size (adjust based on your GPU)
-    project='saved',           # Folder to save results
+    epochs=1,                # You can adjust the number of epochs
+    imgsz=640,               # Image size (default 640)
+    batch=10,                # Batch size (adjust based on your GPU)
+    project='saved',         # Folder to save results
     name='yolov8_uec_food100', # Subfolder name for this run
-    exist_ok=True              # Overwrite if folder exists
+    exist_ok=True            # Overwrite if folder exists
 )
